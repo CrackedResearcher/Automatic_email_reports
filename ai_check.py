9c91e48f679827b358bcd01abfa562d7bd6d947d
Ayush <119443185+CrackedResearcher@users.noreply.github.com>
2024-10-06 19:36:04 +0530

    Update ai_check.py
    changed the model name from flash latest to just flash

--- ai_check.py
+++ ai_check.py
@@ -28,7 +28,7 @@ def flag_and_summarize_email(data, sender):
     
     except ResourceExhausted:
         genai.configure(api_key=os.environ['optional_api_key'])
-        new_model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest",
+        new_model = genai.GenerativeModel(model_name="gemini-1.5-flash",
   system_instruction="You are an email assistant. Your tasks are to summarize email content and filter emails into the following categories: spam, important/alerts, social, work-related, personal/family, and newsletters/promotions. Your output should include:\n\nSummary: Provide a very brief summary of the email content, similar to a personal assistant, e.g., \"This email is about [topic].\"\nFiltered as: Categorize the email into one of the predefined categories, strictly adhering to the given filters.\nCategories and Examples:\n\nSpam (emails pitching services, asking to get on a quick call)\n\nImportant/Alerts (signup, login alerts/codes, bank alerts)\n\nSocial (emails from social media companies)\n\nWork-related:\n\n\"Reminder for our project status meeting scheduled for Monday at 10 AM.\"\n\"Attached is the latest report on our market analysis. Please review and provide feedback.\"\n\nPersonal/Family:\n\n\"Hey John, let's meet for lunch this weekend. There's a new restaurant downtown.\"\n\"Dear Sarah, I got a promotion! Let's celebrate with a family dinner next weekend.\"\n\nNewsletters/Promotions (grouping both):\n\n\"Thank you for subscribing to our newsletter. This week's edition covers the latest trends in technology.\"\n\"Our Flash Sale is live. Get 70% off on selected items for the next 48 hours.\"",
 )
         response = new_model.generate_content(f"heres the email: {data}")
